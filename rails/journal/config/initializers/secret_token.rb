71c94d90668525525b80862c594db8b012fdea5f
xiaoliu8844 <xiaoliu8844@gmail.com>
2013-06-05 23:33:47 +0800

    initial commit

---
+++ rails/journal/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Journal::Application.config.secret_token = '8827f2042db9b0561681f8474e4c777f8463c8d63433a1cc53ddd4195cf0b54b8c9b1a154a1f1d3b60dd529427551e7ea8a551d1ec18b98e293e933306a996dd'
